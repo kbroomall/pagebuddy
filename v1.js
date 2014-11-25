402a5a68cf80c5d446763e33a631e46a684116c1
Broomall, Kevin <kbroomall@pointroll.com>
2014-11-25 15:34:40 -0500

    Initial Commit

--- v1.js
+++ v1.js
@@ -1,69 +1,41 @@
+//writes PointRoll Page Buddy to top of page with line break
 function writeTitle() {
-	document.write("<table>");
-    document.write("<tr>");
-    document.write("<td>");
-	document.write("PointRoll PageBuddy v.09");
-	document.write("</td>");
-    document.write("</tr>");
-    document.write("</table>");
-	document.write("</br>");
+    document.write("<table>", "<tr>", "<td>", "PointRoll PageBuddy v.09", "</td>", "</tr>", "</table>", "</br>");
 }
 writeTitle();
 
 //check flash version and return to window
 function getFlash() {
-var f = navigator.plugins["Shockwave Flash"].description;
-	document.write("<table>");
-    document.write("<tr>");
-    document.write("<td>");
-	document.write("Flash Version:");
-	document.write("</td>");
-	document.write("<td>");
-	document.write(f);
-	document.write("</td>");
-    document.write("</tr>");
-    document.write("</table>");
+    var f = navigator.plugins["Shockwave Flash"].description;
+    document.write("<table>", "<tr>", "<td>", "Flash Version:", "</td>", "<td>", f, "</td>", "</tr>", "</table>");
 }
 getFlash();
 
+//checks to see if page is SSL compliant...
 function getSecure() {
-if (location.protocol.toLowerCase().indexOf("https") >= 0) {
-	document.write("<table>");
-    document.write("<tr>");
-    document.write("<td>");
-	document.write("Secure Protocol:");
-	document.write("</td>");
-	document.write("<td>");
-	document.write("Page Secure");
-	document.write("</td>");
-    document.write("</tr>");
-    document.write("</table>");
-}else{
-	document.write("<table>");
-    document.write("<tr>");
-    document.write("<td>");
-	document.write("Secure Protocol:");
-	document.write("</td>");
-	document.write("<td>");
-	document.write("No SSL protocol detected...");
-	document.write("</td>");
-    document.write("</tr>");
-    document.write("</table>");
-}}
+    if (location.protocol.toLowerCase().indexOf("https") >= 0) {
+        document.write("<table>", "<tr>", "<td>", "Secure Protocol:", "</td>", "<td>", "Page Secure", "</td>", "</tr>", "</table>");
+    } else {
+        document.write("<table>", "<tr>", "<td>", "Secure Protocol:", "</td>", "<td>", "SSL Protocol not detected...", "</td>", "</tr>", "</table>");
+    }
+}
 getSecure();
-  
+
 //check to see how many iframes exist on the page:
 function getIframes() {
     var x = document.getElementsByTagName("iframe");
-	document.write("<table>");
-    document.write("<tr>");
-    document.write("<td>");
-	document.write("Iframes on Page:");
-	document.write("</td>");
-	document.write("<td>");
-	document.write(x.length);
-	document.write("</td>");
-    document.write("</tr>");
-    document.write("</table>");
+    document.write("<table>", "<tr>", "<td>", "Iframes on Page:", "</td>", "<td>", x.length, "</td>", "</tr>", "</table>");
 }
-getIframes(); 
+getIframes();
+
+//checks to see if location.href matches document.domain:
+function getDomains() {
+    var str1 = location.href.split('/')[2],
+        str2 = document.domain;
+    if (str1 == str2) {
+        document.write("<table>", "<tr>", "<td>", "loc.href vs doc.dom:", "</td>", "<td>", "MATCH", "</td>", "</tr>", "</table>");
+    } else {
+        document.write("<table>", "<tr>", "<td>", "loc.href vs doc.dom:", "</td>", "<td>", "MISMATCH", "</td>", "</tr>", "</table>");
+    }
+}
+getDomains();
--- v2.js
+++ v2.js
@@ -20,6 +20,41 @@ function getDomains()
 }
 
 
+//error in this version:
+
+NOT WORKING
+//check to see if location.href matches document.domain
+function getDomains() {
+  var str1 = location.href.split('/')[2];
+  var str2 = document.domain;
+    if(str1 == str2)
+    {
+		document.write("<table>");
+		document.write("<tr>");
+		document.write("<td>");
+		document.write("Domains:");
+		document.write("</td>");
+		document.write("<td>");
+		document.write("Match);
+		document.write("</td>");
+		document.write("</tr>");
+		document.write("</table>");
+    }else{
+    	document.write("<table>");
+		document.write("<tr>");
+		document.write("<td>");
+		document.write("Domains:");
+		document.write("</td>");
+		document.write("<td>");
+		document.write("MISMATCH);
+		document.write("</td>");
+		document.write("</tr>");
+		document.write("</table>");
+}}
+getDomains();
+
+
+
 //check to see if the page secure?
 
 function getSecure() {
